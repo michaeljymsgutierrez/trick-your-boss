cef77e17242da1e3b239a0dba7ec0632ccdd1fc1
michaeljymsgutierrez <leeez711@gmail.com>
2025-01-30 12:54:50 +0800

    Revised logic

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,16 @@
+function triggerKeepOnline() {
+  let count = 1
+
+  setInterval(function () {
+    if (count % 2 === 0) {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+    } else {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+    count++
+  }, count * 1000)
+}
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'scrollUp') {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  } else if (request.action === 'scrollDown') {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
-  }
+  if (request.action === 'enableKeepOnline') triggerKeepOnline()
 })
--- popup.js
+++ popup.js
@@ -1,29 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const btnUp = document.getElementById('btn-up')
-  const btnDown = document.getElementById('btn-down')
   const btnEnable = document.getElementById('enable-btn')
 
   btnEnable.addEventListener('click', function () {
-    let count = 1
-    setInterval(function () {
-      if (count % 2 === 0) {
-        btnDown.click()
-      } else {
-        btnUp.click()
-      }
-      count++
-    }, count * 1000)
-  })
-
-  btnUp.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'scrollUp' })
-    })
-  })
-
-  btnDown.addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'scrollDown' })
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'enableKeepOnline' })
     })
   })
 })
