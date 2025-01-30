4c64a2bfbefa32457c9e06ce960b3668d8905f0b
michaeljymsgutierrez <leeez711@gmail.com>
2025-01-30 11:23:12 +0800

    Updated interface

--- popup.js
+++ popup.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const btnUp = document.getElementById('btn-up')
   const btnDown = document.getElementById('btn-down')
+  const btnEnable = document.getElementById('enable-btn')
+
+  btnEnable.addEventListener('click', function () {
+    let count = 1
+    setInterval(function () {
+      if (count % 2 === 0) {
+        btnDown.click()
+      } else {
+        btnUp.click()
+      }
+      count++
+    }, count * 1000)
+  })
 
   btnUp.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
